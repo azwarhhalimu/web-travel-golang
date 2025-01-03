6c87f854e652c5964ca550d54810006c508b8761
AZWAR HALIMU <azwarhalimu@gmail.com>
2025-01-03 10:09:58 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"web_traveler/app/middleware"
+	"web_traveler/app/routes"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/jet/v2"
+)
+
+func main() {
+
+	template := jet.New("./app/views", ".jet")
+
+	app := fiber.New(fiber.Config{
+		Views: template,
+	})
+	public := app.Group("/").(*fiber.Group)
+	routes.PubliRouting(public)
+
+	admin := app.Group("/admin", middleware.AuthAdmin).(*fiber.Group)
+	routes.AdminRouting(admin)
+
+	log.Panic(app.Listen(":2000"))
+
+}
